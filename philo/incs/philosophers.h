d73ea1497db32bbf871d5799a1c1b4f4a51d3098
pettei47 <kitagawa.teppei.33c@gmail.com>
2022-01-03 19:31:16 +0900

    create:philosophers.c/h

---
+++ philo/incs/philosophers.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: teppei <teppei@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/03 19:26:53 by teppei            #+#    #+#             */
+/*   Updated: 2022/01/03 19:30:22 by teppei           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <pthread.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+
+
+#endif
