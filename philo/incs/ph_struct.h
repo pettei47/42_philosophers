f8d86a28f7fbffdf12300d17ff7ea434e6a3ffde
pettei47 <kitagawa.teppei.33c@gmail.com>
2022-01-05 01:47:57 +0900

    ph_init_philos

--- philo/incs/ph_struct.h
+++ philo/incs/ph_struct.h
@@ -6,7 +6,7 @@
 /*   By: teppei <teppei@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 19:54:45 by teppei            #+#    #+#             */
-/*   Updated: 2022/01/03 21:34:40 by teppei           ###   ########.fr       */
+/*   Updated: 2022/01/05 01:43:55 by teppei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,18 @@
 
 # include "philosophers.h"
 
+typedef struct s_god	t_god;
+typedef struct s_philo
+{
+	unsigned long	num;
+	unsigned long	l_fork;
+	unsigned long	r_fork;
+	unsigned long	time_have_eaten;
+	unsigned long	eat_count;
+	t_god			g;
+}	t_philo;
+
+
 typedef struct s_god
 {
 	unsigned long	num_of_philos;
@@ -22,7 +34,38 @@ typedef struct s_god
 	unsigned long	time_to_eat;
 	unsigned long	time_to_sleep;
 	unsigned long	num_of_must_eat;
+	pthread_mutex_t	*forks;
+	t_philo			*ph;
 }	t_god;
+
+// typedef struct s_data	t_data;
+// typedef struct s_philo
+// {
+// 	int				eat_count;
+// 	int				num;
+// 	int				l_fork;
+// 	int				r_fork;
+// 	size_t			last_eat_time;
+// 	pthread_t		thread;
+// 	t_data			*d;
+// }	t_philo;
+
+// typedef struct s_data
+// {
+// 	int				philo_num;
+// 	size_t			die_time;
+// 	size_t			eat_time;
+// 	size_t			sleep_time;
+// 	int				max_eat_num;
+// 	int				fork_num;
+// 	int				end_flag;
+// 	int				philos_eat_count;
+// 	pthread_mutex_t	*fork;
+// 	pthread_mutex_t	common_protect;
+// 	pthread_t		starvation_monitor;
+// 	t_philo			*philo;
+// }	t_data;
+
 
 
 #endif
--- philo/src/philosophers.c
+++ philo/src/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: teppei <teppei@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 19:24:46 by teppei            #+#    #+#             */
-/*   Updated: 2022/01/03 21:52:44 by teppei           ###   ########.fr       */
+/*   Updated: 2022/01/05 01:44:27 by teppei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,6 +77,27 @@ bool	ph_check_args(char **av, t_god *god)
 	return (true);
 }
 
+bool	ph_init_philos(t_god *god)
+{
+	long	i;
+
+	god->ph = (t_philo *)malloc(sizeof(t_philo) * god->num_of_philos);
+	if (!god->ph)
+		return (false);
+	i = -1;
+	while (++i < god->num_of_philos)
+	{
+		god->ph[i].num = i + 1;
+		god->ph[i].l_fork = i;
+		if (i + 1 == god->num_of_philos)
+			god->ph[i].r_fork = 0;
+		else
+			god->ph[i].r_fork = i + 1;
+		god->ph[i].time_have_eaten = 0;
+		god->ph[i].eat_count = 0;
+	}
+}
+
 int	main(int ac, char **av)
 {
 	t_god	god;
@@ -85,5 +106,7 @@ int	main(int ac, char **av)
 		return (ph_error(1, USAGE));
 	if (!ph_check_args(av, &god))
 		return (ph_error(1, E_ARGS));
+	if (!ph_init_philos(&god))
+		return (ph_error(1, "failed to init_philo"));
 	return (0);
 }
